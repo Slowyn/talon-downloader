7abcd55a99a3352c384bc8158aa3c6385db3d9fc
Konstantin Iakushin <y231966mdk@gmail.com>
2025-01-02 02:08:52 +0300

    Translations for statuses

--- src/renderer/Downloads/columns.tsx
+++ src/renderer/Downloads/columns.tsx
@@ -5,8 +5,27 @@ export type DownloadRow = {
     id: string;
     status: DownloadStatus['status'];
 };
+function mapStatusToLabel(status: DownloadStatus['status']) {
+    const dict: Record<DownloadStatus['status'], string> = {
+        NotStarted: 'Не скачан',
+        InProgress: 'Скачивается',
+        Completed: 'Скачан',
+        Failed: 'Произошла ошибка',
+    };
+
+    return dict[status];
+}
 
 export const columns: ColumnDef<DownloadRow>[] = [
-    {accessorKey: 'id', header: 'Номер талона'},
-    {accessorKey: 'status', header: 'Статус'},
+    {
+        accessorKey: 'id',
+        header: 'Номер талона',
+    },
+    {
+        accessorKey: 'status',
+        header: () => <div className="text-right">Статус</div>,
+        cell: ({row}) => {
+            return <div className="text-right font-medium">{mapStatusToLabel(row.original.status)}</div>;
+        },
+    },
 ];
